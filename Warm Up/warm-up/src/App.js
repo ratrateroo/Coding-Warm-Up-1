898607b004b5c755162cf290e62ae21f40e12d7b
Mark Leo <ratrateroo@gmail.com>
2020-01-23 22:42:30 +0800

    used props to pass data between components

--- Warm Up/warm-up/src/App.js
+++ Warm Up/warm-up/src/App.js
@@ -5,10 +5,16 @@ import './App.css';
 
 //Function Based Components
 const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+  ];
+
   return (
     <div className="course-goals">
       <h2>Course Goals</h2>
-      <GoalList />
+      <GoalList goals={courseGoals} />
     </div>
   );
 };
