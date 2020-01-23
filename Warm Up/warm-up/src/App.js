54865e4fad7c461e86b87111d38286a2957d81e3
Mark Leo <ratrateroo@gmail.com>
2020-01-23 21:26:02 +0800

    understanding jsx

--- Warm Up/warm-up/src/App.js
+++ Warm Up/warm-up/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  //return <h1>A React App!</h1>;
+  return React.createElement('h1', {}, 'Hi, this is React');
 };
 
 export default App;
