cdf1c9f07e8798a0f4fa33e95bde70ecc9e2593f
Mark Leo <ratrateroo@gmail.com>
2020-01-23 21:34:46 +0800

    understanding components

--- Warm Up/warm-up/src/App.js
+++ Warm Up/warm-up/src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
+//Function Based Components
 const App = () => {
-  //return <h1>A React App!</h1>;
-  return React.createElement('h1', {}, 'Hi, this is React');
+  //return React.createElement('h1', {title: "This works"}, 'Hi, this is ReactJS!');
+  return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
 };
+
+//Class Based Components
+/* class App extends React.Component {
+  render() {
+    return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>; 
+  }
+} */
 
 export default App;
